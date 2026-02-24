4ec0abaf3ecab5d1029ac3a4522e045622af745b
Nakul Chouksey <71.nakul@gmail.com>
2026-02-24 23:54:26 +0530

    refactor: new app

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,65 @@
-"use client";
+import Image from "next/image";
 
-import QrScanner from "@/components/QRScanner";
-import Receive from "@/components/Receive";
-import { Send } from "lucide-react";
-import { useState } from "react";
-const page = () => {
-  const [send, setSend] = useState(true);
-
+export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-8 bg-black">
-      <div className="w-full max-w-4xl mx-auto">
-        <div className="flex justify-around w-full">
-          <button className="text-zinc-200 text-3xl" onClick={() => setSend(true)}>Send</button>
-          <button className="text-zinc-200 text-3xl" onClick={() => setSend(false)}>Receive</button>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            To get started, edit the page.tsx file.
+          </h1>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Looking for a starting point or more instructions? Head over to{" "}
+            <a
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Templates
+            </a>{" "}
+            or the{" "}
+            <a
+              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Learning
+            </a>{" "}
+            center.
+          </p>
         </div>
-        <div className="w-full h-[1px] bg-zinc-600 m-4"></div>
-        {/* {send ? <Send/> : <Receive/>} */}
-        <QrScanner /> 
-      </div>
-    </main>
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <a
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={16}
+              height={16}
+            />
+            Deploy Now
+          </a>
+          <a
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Documentation
+          </a>
+        </div>
+      </main>
+    </div>
   );
-};
-
-export default page;
+}
--- components/Receive.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Receive = () => {
-  return (
-    <div>Receive</div>
-  )
-}
-
-export default Receive
